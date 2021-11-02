3d2121df3f8c9e5f29c4f6aa59592a41119bde8b
Masoud <masoudrezaei86@gmail.com>
2021-11-02 14:50:02 +0330

    Done

--- Webdav-crio-python.py
+++ Webdav-crio-python.py
@@ -10,6 +10,13 @@ import win32wnet
 win32wnet.WNetAddConnection2(1, 'Z:', r'http://hostIPAddress/files/', None, 'username', 'password')
 
 
+# if there is no error in line 10, Map Network drive is created successfully.
+# otherwise, you should revise the inputs, 
+# there are some possible reasons,
+# 1- NI-cRIO is not connected to Host PC, you can check it using NI-MAX software.
+# 2- NI-cRIO IP, username or password is wrong that can be checked with NI-MAX software.
+
+
 crio_file = open(r"Z:\C\test2.txt", "rt")
 #read file contents to string
 data = crio_file.read()
